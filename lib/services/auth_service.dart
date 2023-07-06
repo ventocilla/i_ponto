ea563e3dc12d0e20905739a7204d398b9d656fe2
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2023-07-05 21:49:05 -0300

    added auth service class

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthService extends ChangeNotifier {
+  final SupabaseClient _supabase = Supabase.instance.client;
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  set setIsLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
+}
