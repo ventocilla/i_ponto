b682be5da3618e38354bea8e6a08253eb2999fc6
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2023-07-06 06:30:15 -0300

    added UserModel

---
+++ lib/models/user_model.dart
@@ -0,0 +1,26 @@
+class UserModel {
+  final String id;
+  final String email;
+  final String name;
+  final int? department;
+  final String employeeId;
+
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.name,
+    this.department,
+    required this.employeeId,
+  });
+
+  // Convert JsonData to Model ..
+  factory UserModel.fromJson(Map<String, dynamic> data) {
+    return UserModel(
+      id: data['id'],
+      email: data['email'],
+      name: data['name'],
+      department: data['department'],
+      employeeId: data['employeeId'],
+    );
+  }
+}
