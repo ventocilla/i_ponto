3ccc6ce6b1fa063be7fd92d5004cc2d17329f5d6
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2023-07-07 14:51:44 -0300

    work on Attendance UI

--- lib/screens/attendence_screen.dart
+++ lib/screens/attendence_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class AttendanceScreen extends StatelessWidget {
@@ -5,9 +7,52 @@ class AttendanceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Attendance'),
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 32),
+              child: const Text(
+                'Wellcome',
+                style: TextStyle(color: Colors.black54, fontSize: 30),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Employee Name',
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 32),
+              child: const Text(
+                "Today's status",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 12, bottom: 32),
+              height: 150,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    offset: Offset(2, 2),
+                  ),
+                ],
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              //
+            ),
+          ],
+        ),
       ),
     );
   }
