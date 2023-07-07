a8789fcc28d39aa370fff11988a0b6c74f3c7398
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2023-07-07 08:44:29 -0300

    added bottom navigation bar in home.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +9,42 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<IconData> navigationIcons = [
+    FontAwesomeIcons.solidCalendarDays,
+    FontAwesomeIcons.check,
+    FontAwesomeIcons.solidUser,
+  ];
+
+  int currentIndex = 1;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const Center(
         child: Text('Home Screen'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            for (int i = 0; i < navigationIcons.length; i++) ...{
+              Expanded(
+                  child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    currentIndex = i;
+                  });
+                },
+                child: Center(
+                  child: FaIcon(
+                    navigationIcons[i],
+                    color: i == currentIndex ? Colors.red : Colors.grey,
+                    size: i == currentIndex ? 30 : 26,
+                  ),
+                ),
+              ))
+            }
+          ],
+        ),
       ),
     );
   }
