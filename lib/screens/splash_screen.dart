4bf0cbc6a1e9d45237a38f27e7b522a993838406
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2023-07-06 23:29:43 -0300

    added splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:i_ponto/screens/home_screen.dart';
+import 'package:i_ponto/screens/login_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../services/auth_service.dart';
+
+class SpalshScreen extends StatelessWidget {
+  const SpalshScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+    authService.signOut();
+    return authService.currentUser == null
+        ? const LoginScreen()
+        : const HomeScreen();
+  }
+}
