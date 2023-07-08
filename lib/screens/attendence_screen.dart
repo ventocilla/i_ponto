d69841961021b3913626d2d32fb5064b7cd18e7d
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2023-07-07 22:47:46 -0300

    implemented slide to check in

--- lib/screens/attendence_screen.dart
+++ lib/screens/attendence_screen.dart
@@ -1,15 +1,25 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import 'package:slide_to_confirm/slide_to_confirm.dart';
 
-class AttendanceScreen extends StatelessWidget {
+// https://pub.dev/packages/slide_to_confirm
+
+class AttendanceScreen extends StatefulWidget {
   const AttendanceScreen({super.key});
+
+  @override
+  State<AttendanceScreen> createState() => _AttendanceScreenState();
+}
+
+class _AttendanceScreenState extends State<AttendanceScreen> {
+  void confirmed() {
+    //print('Slider confirmed!');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: Column(
           children: [
             Container(
@@ -36,7 +46,7 @@ class AttendanceScreen extends StatelessWidget {
               ),
             ),
             Container(
-              margin: const EdgeInsets.only(top: 12, bottom: 32),
+              margin: const EdgeInsets.only(top: 20, bottom: 32),
               height: 150,
               decoration: const BoxDecoration(
                 color: Colors.white,
@@ -49,7 +59,68 @@ class AttendanceScreen extends StatelessWidget {
                 ],
                 borderRadius: BorderRadius.all(Radius.circular(20)),
               ),
-              //
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Check In',
+                            style:
+                                TextStyle(fontSize: 20, color: Colors.black54)),
+                        SizedBox(width: 20),
+                        Text('09:30', style: TextStyle(fontSize: 25)),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Check Out',
+                            style:
+                                TextStyle(fontSize: 20, color: Colors.black54)),
+                        SizedBox(width: 20),
+                        Text('--/--', style: TextStyle(fontSize: 25)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                '15 April 2023',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                '20:00:01 PM',
+                style: TextStyle(fontSize: 15, color: Colors.black54),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 25),
+              child: Builder(
+                builder: (context) {
+                  return ConfirmationSlider(
+                    text: 'Slide to Check',
+                    textStyle: const TextStyle(
+                      color: Colors.black54,
+                      fontSize: 18,
+                    ),
+                    onConfirmation: () => confirmed(),
+                    foregroundColor: Colors.redAccent, // default: blueAccent
+                  );
+                },
+              ),
             ),
           ],
         ),
